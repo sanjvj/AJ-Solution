0e06e535c7e86c4c1813f6984991fc45e01d5b88
sanjvj <sanjaysurya3010@gmail.com>
2024-07-18 00:45:20 +0530

    Add Services description

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,5 +1,4 @@
 import { Footer } from "../Components/Footer";
-import { Topbar } from "../Components/Topbar";
 import {ServiceIntro} from "../Components/ServiceIntro";
 import { ServiceHero } from "../Components/ServiceHero";
 import { ServiceCard } from "../Components/ServiceCard";
@@ -7,7 +6,6 @@ import {FooterAnimation} from "../Components/FooterAnimation"
 export function Services() {
   return (
     <div>
-      <Topbar></Topbar>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <ServiceIntro></ServiceIntro>
       <ServiceHero></ServiceHero>
