d85fcd28115f1336b8905f42ab7248a6d99dc11b
sanjvj <sanjaysurya3010@gmail.com>
2024-07-22 02:46:10 +0530

    Changes in Services page

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -3,9 +3,11 @@ import {ServiceIntro} from "../Components/ServiceIntro";
 import { ServiceHero } from "../Components/ServiceHero";
 import { ServiceCard } from "../Components/ServiceCard";
 import {FooterAnimation} from "../Components/FooterAnimation"
+import { Topbar } from "../Components/Topbar";
 export function Services() {
   return (
     <div>
+    <Topbar></Topbar>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <ServiceIntro></ServiceIntro>
       <ServiceHero></ServiceHero>
